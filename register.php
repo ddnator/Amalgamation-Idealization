ddf8a120b54388312d5ad88f8fe96e24b05ab90c
ddnator <wangdarren3@gmail.com>
2026-09-21 14:34:16 +0200

    fixed wrong user send to database

--- register.php
+++ register.php
@@ -14,18 +14,23 @@ if (isset($_SESSION['logged_in'])) {
 
 if (isset($_POST['submit'])) {
     require_once "includes/database.php";
-    $user = mysqli_real_escape_string($db, $_POST['userName']);
+    $userName = mysqli_real_escape_string($db, $_POST['userName']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
     $banknumber = mysqli_real_escape_string($db, $_POST['banknumber']);
     //check if user already exists
-
+    $query = "SELECT * FROM `users` WHERE `username` = '$userName';";
+    $result = mysqli_query($db, $query);
+    $user = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    if(!empty($user)) {
+        //
+    } else {
+        $securePassword = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO `users`(`banknumber`, `password`, `username`) VALUES ('$banknumber','$securePassword', '$userName')";
+        mysqli_query($db, $query);
+        header("location: login.php");
+    }
 
     //
-
-    $securePassword = password_hash($password, PASSWORD_DEFAULT);
-    $query = "INSERT INTO `users`(`username`, `password`) VALUES ('$user','$securePassword')";
-    mysqli_query($db, $query);
-    header("location: login.php");
 }
 ?>
 
@@ -45,15 +50,15 @@ if (isset($_POST['submit'])) {
 
    <form method="post">
         <input type="text" id="userName" name="userName" value="" required>
-    <label for="userName"></label>
+        <label for="userName"></label>
 
-    <input type="text" id="password" name="password" value="" required>
-    <label for="password"></label>
+        <input type="text" id="password" name="password" value="" required>
+        <label for="password"></label>
 
-    <input type="text" id="banknumber" name="banknumber" value="" required>
-    <label for="banknumber"></label>
+        <input type="text" id="banknumber" name="banknumber" value="" required>
+        <label for="banknumber"></label>
 
-    <input type="submit" name="submit">
+        <input type="submit" name="submit">
    </form>
 
    <a href="login.php">Already have an account?</a>
