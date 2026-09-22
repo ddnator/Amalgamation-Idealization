157ddd46fc78930202e78ef29ce852b5f8059edd
ddnator <wangdarren3@gmail.com>
2026-09-22 10:13:18 +0200

    added validation

--- login.php
+++ login.php
@@ -11,13 +11,28 @@ if (isset($_SESSION['logged_in'])) {
 }
 if(isset($_POST['submit'])) {
     require_once "includes/database.php";
-    $userName = mysqli_real_escape_string($db, $_POST['userName']);
+    $username = mysqli_real_escape_string($db, $_POST['username']);
     $givenPassword = mysqli_real_escape_string($db, $_POST['password']);
 
     //validation
+    if ($username === '') {
+        $errors['username'] = 'No username has been entered';
+    } else {
+        if (strlen($username) > 30) {
+            $errors['username'] = 'Username is too long';
+        }
+    }
+
+    if ($password === '') {
+        $errors['password'] = 'No password has been entered';
+    } else {
+        if (strlen($password) > 30 || strlen($password) < 8) {
+            $errors['password'] = 'Please select a password length between 8 and 30 characters';
+        } 
+    }
 
     if(empty($errors)) {
-        $query = "SELECT * FROM `users` WHERE `userName` = '$userName';";
+        $query = "SELECT * FROM `users` WHERE `username` = '$username';";
         $result = mysqli_query($db, $query);
         $user = mysqli_fetch_all($result, MYSQLI_ASSOC);
         if(empty($user)) {
@@ -26,7 +41,7 @@ if(isset($_POST['submit'])) {
         } else {
             $userPassword = $user[0]['password'];
             if(password_verify($givenPassword, $userPassword)) {;
-                $_SESSION['user_email'] = $user[0]['username'];
+                $_SESSION['username'] = $user[0]['username'];
                 $_SESSION['logged_in'] = true;
                 header("location: index.php");
                 exit;
@@ -51,17 +66,20 @@ if(isset($_POST['submit'])) {
    
     <h1>Login</h1>
    
-
+    <?php if (!$login) { ?>
    <form method="post">
-        <input type="text" id="userName" name="userName" value='<?= htmlentities($userName ?? '')  ?>' required>
-    <label for="userName">username</label>
+        <label for="username">username</label>
+        <input type="text" id="username" name="username" value='<?= htmlentities($username ?? '')  ?>' required>
 
-    <input type="text" id="password" name="password" value="" required>
-    <label for="password">password</label>
+        <label for="password">password</label>
+        <input type="password" id="password" name="password" value="" required>
 
-    <input type="submit" name="submit">
+        <input type="submit" name="submit">
    </form>
-
    <a href="register.php">register</a>
+    <?php } else { ?>
+        <p>You are already logged in!</p>
+        <p><a href="logout.php">Log out</a> / <a href="index.php">To home page</a></p>
+    <?php } ?>
 </body>
 </html>
--- register.php
+++ register.php
@@ -7,29 +7,56 @@ $errors = [];
 if (isset($_SESSION['logged_in'])) {
     if ($_SESSION['logged_in'] === true) {
         $login = true;
+        header("location: index.php");
     }
 }
 
-//validation
-
 if (isset($_POST['submit'])) {
     require_once "includes/database.php";
-    $userName = mysqli_real_escape_string($db, $_POST['userName']);
+    $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
     $banknumber = mysqli_real_escape_string($db, $_POST['banknumber']);
-    //check if user already exists
-    $query = "SELECT * FROM `users` WHERE `username` = '$userName';";
-    $result = mysqli_query($db, $query);
-    $user = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    if(!empty($user)) {
-        //
+    //
+    if ($username === '') {
+        $errors['username'] = 'No username has been entered';
     } else {
-        $securePassword = password_hash($password, PASSWORD_DEFAULT);
-        $query = "INSERT INTO `users`(`banknumber`, `password`, `username`) VALUES ('$banknumber','$securePassword', '$userName')";
-        mysqli_query($db, $query);
-        header("location: login.php");
+        if (strlen($username) > 30) {
+            $errors['username'] = 'Username is too long';
+        }
     }
 
+    if ($password === '') {
+        $errors['password'] = 'No password has been entered';
+    } else {
+        if (strlen($password) > 30 || strlen($password) < 8) {
+            $errors['password'] = 'Please select a password length between 8 and 30 characters';
+        } 
+    }
+
+    if ($banknumber === '') {
+        $errors['banknumber'] = 'No banknumber has been entered';
+    } else {
+        if (strlen($banknumber) > 34) {
+            $errors['banknumber'] = 'Maximum size of a banknumber can only be 34';
+        } else if (strlen($banknumber) < 4) {
+            $errors['banknumber'] = 'Banknumber too small';
+        }
+    }
+
+    if (empty($errors)){
+    //check if user already exists
+        $query = "SELECT * FROM `users` WHERE `username` = '$username';";
+        $result = mysqli_query($db, $query);
+        $user = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        if(!empty($user)) {
+            
+        } else {
+            $securePassword = password_hash($password, PASSWORD_DEFAULT);
+            $query = "INSERT INTO `users`(`banknumber`, `password`, `username`) VALUES ('$banknumber','$securePassword', '$username')";
+            mysqli_query($db, $query);
+            header("location: login.php");
+        }
+    }
     //
 }
 ?>
@@ -49,14 +76,23 @@ if (isset($_POST['submit'])) {
    
 
    <form method="post">
-        <input type="text" id="userName" name="userName" value="" required>
-        <label for="userName"></label>
+        <input type="text" id="username" name="username" value="<?= htmlentities($username ?? '' )?>" required>
+        <label for="username"></label>
+        <p class="help is-danger">
+            <?= $errors['username'] ?? '' ?>
+        </p>
 
-        <input type="text" id="password" name="password" value="" required>
+        <input type="text" id="password" name="password" value="<?= htmlentities($password ?? '' )?>" required>
         <label for="password"></label>
+        <p class="help is-danger">
+            <?= $errors['password'] ?? '' ?>
+        </p>
 
-        <input type="text" id="banknumber" name="banknumber" value="" required>
+        <input type="text" id="banknumber" name="banknumber" value="<?= htmlentities($banknumber ?? '' )?>" required>
         <label for="banknumber"></label>
+        <p class="help is-danger">
+            <?= $errors['banknumber'] ?? '' ?>
+        </p>
 
         <input type="submit" name="submit">
    </form>
